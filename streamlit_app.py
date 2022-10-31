8f95ddc2168b860682f9b175eacba5cc2007dc92
deepuyarli <117097577+Deepuyarli@users.noreply.github.com>
2022-10-31 10:16:58 -0500

    Update streamlit_app.py
    i added menu to page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title('My parentsnew healthy diner');
+streamlit.title('My parents new healthy diner');
+
+streamlit.header('Breakfast Menu');
+streamlit.text('Omega 3 & blueberry oatmeal');
+streamlit.text('Kale,spinach & rocket smoothie');
+stremlit.text('Hard boiled free range egg');
