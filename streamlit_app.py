97a1b26e31b9d02a993f22359564b4cc3054abc9
deepuyarli <117097577+Deepuyarli@users.noreply.github.com>
2022-10-31 12:41:37 -0500

    Update streamlit_app.py
    Added another header to menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,4 @@ streamlit.text('🥣 Omega 3 & blueberry oatmeal');
 streamlit.text('🥗Kale,spinach & rocket smoothie');
 streamlit.text('🐔Hard boiled free range egg');
 streamlit.text('🥑🍞Avocado toast');
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
