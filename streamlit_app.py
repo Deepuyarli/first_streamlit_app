c870de275c736d7fb45bad65f4fc7fdb4d76ab2a
deepuyarli <117097577+Deepuyarli@users.noreply.github.com>
2022-10-31 13:51:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My parents new healthy diner');
+streamlit.title('My Moms new healthy diner');
 
 streamlit.header('Breakfast Menu');
 streamlit.text('🥣 Omega 3 & blueberry oatmeal');
